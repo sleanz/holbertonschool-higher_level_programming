fb9002738a45872a1089b61c59d4d6811704c70a
sleanz <slean@sleanpc>
2025-05-13 12:17:17 +0200

    data strcuture

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,2 @@
+def divisible_by_2(my_list=[]):
+    return [number % 2 == 0 for number in my_list]
---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        copy_list = my_list.copy()
+        if idx >= 0 and idx < len(my_list):
+            copy_list[idx] = element
+        return copy_list
+    return my_list
