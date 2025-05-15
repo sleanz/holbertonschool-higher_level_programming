8b8088f8f172609a987be22d011f6ce394c7c8bd
sleanz <slean@sleanpc>
2025-05-15 12:20:29 +0200

    pycode

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """
-    Remplace toutes les occurrences d'un élément par un autre dans une nouvelle liste.
-    
-    Args:
-        my_list: La liste initiale
-        search: L'élément à remplacer dans la liste
-        replace: Le nouvel élément
-        
-    Returns:
-        Une nouvelle liste avec les remplacements effectués
-    """
-    # Crée une nouvelle liste en utilisant une compréhension de liste
     new_list = [replace if element == search else element for element in my_list]
-    
     return new_list
