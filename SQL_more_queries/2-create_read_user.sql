390a7559f941451f5f331ab26eb0d97ab1b3b186
sleanz <slean@sleanpc>
2025-06-26 11:11:36 +0200

    correction 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,14 @@
+-- Script to create database hbtn_0d_2 and user user_0d_2 with SELECT privileges
+-- Usage: mysql < create_user_and_database.sql
+
+-- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the database to the user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply the privilege changes
 FLUSH PRIVILEGES;
