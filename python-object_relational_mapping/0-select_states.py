9aba16173ef0e387f58bc40b6ebca96d9f202911
sleanz <slean@sleanpc>
2025-06-26 11:29:31 +0200

    essaie

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,46 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa
-Usage: python3 0-select_states.py <mysql_username> <mysql_password> <database_name>
-"""
+"""lists all states from the database hbtn_0e_0_usa"""
 
-import MySQLdb
-import sys
+if __name__ == '__main__':
 
+    import MySQLdb
+    import sys
 
-def list_states():
-    """Connect to MySQL database and list all states"""
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    
-    # Create cursor object
-    cursor = db.cursor()
-    
-    # Execute SQL query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    
-    # Fetch all results
-    results = cursor.fetchall()
-    
-    # Display results
-    for row in results:
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-    
-    # Close database connection
-    cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    list_states()
